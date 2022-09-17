540fdb7611189192a44198222eacd571b97c6a5d
xl-mohsen <mohsen.rezvani.rad33@gmail.com>
2022-09-17 14:48:35 +0430

    add 404 page, react-icons

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+// import FaExclamationTriangle from "react-icons";
+import Link from "next/link";
+import React from "react";
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+import { BsExclamationTriangle } from "react-icons/bs";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not found">
+      <div className={styles.error}>
+        <h1>
+          <BsExclamationTriangle />
+          404
+        </h1>
+        <h4>Page not found</h4>
+        <Link href="/">
+          <a>Go back home</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
